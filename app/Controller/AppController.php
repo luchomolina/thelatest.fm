f2f2da92cbf6f637c63eb58c9d01b1a79beba851
nicolas_bohorquez <nicolas.bg@gmail.com>
2012-05-28 17:52:21 -0500

    Added sample methods to show PMD metrics

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,4 +32,330 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	
+	// this is bad because it is PHP 4 style
+    public function AppController() {}
+	const myTest = ""; // should be in UPPERCASE
+	
+	/*Code Size*/
+	
+	//unused vars 
+	public function unusedVard() {
+		$var1 = '';
+	}
+	
+	//too many decision points
+	public function complexity($a,$b,$c,$d,$e,$f,$z) {
+		if ($a == $b)  {
+           if ($a1 == $b1) {
+                fiddle();
+           } else if ($a2 == $b2) {
+                fiddle();
+            }  else {
+                fiddle();
+            }
+       } else if ($c == $d) {
+           while ($c == $d) {
+                fiddle();
+            }
+        } else if ($e == $f) {
+           for ($n = 0; $n < $h; $n++) {
+                fiddle();
+            }
+        } else{
+            switch ($z) {
+               case 1:
+                    fiddle();
+                    break;
+              case 2:
+                    fiddle();
+                    break;
+              case 3:
+                    fiddle();
+                    break;
+              default:
+                    fiddle();
+                    break;
+            }
+        }
+	}
+	
+	//method does too much
+	public function tooLong() {
+        print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+    	print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+    	print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+		print("Hello world!" . PHP_EOL);
+        print("Hello world!" . PHP_EOL);
+	}
+
+	/*Design*/
+	
+	//untesteable
+	public function exitCode($param)  {
+        if ($param === 42) {
+            exit(23);
+        }
+    }
+	
+	//untesteable, sec risk
+	public function evalExpression($param)  {
+        if ($param === 42) {
+            eval('$param = 23;');
+        }
+    }
+	
+	// should be isFoo
+	public function getFoo() {true;} 
 }
